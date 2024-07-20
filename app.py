abbf4f076ea3e78f5367011630d26eba24075bff
seobohdanov <seobohdanov@gmail.com>
2024-07-20 16:21:13 +0200

    Добавил23

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ def remove_photo():
             if 0 <= photo_index < len(session['orders'][order_index]['photos']):
                                 session['orders'][order_index]['photos'].pop(photo_index)
             if len(session['orders'][order_index]['photos']) == 0:
-                    session['orders'].pop(order_index)
+                session['orders'].pop(order_index)
                 session.modified = True
                 return jsonify(success=True)
         return jsonify(success=False, error='Invalid index')
